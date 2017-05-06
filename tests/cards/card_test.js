684603cceddecb9215d20f10aebd2e6ff0154e9f
marek kacprzak <marek.kacprzak@windowslive.com>
2017-05-06 21:14:43 +0100

    initialize from course source - but translate some name into polish word (cards)

---
+++ tests/cards/card_test.js
@@ -0,0 +1,18 @@
+describe("A card", function () {
+  "use strict";
+
+  it("should link to the image of the suit and rank", function () {
+      expect(new Card({ rank: "King", figura: "Pik"}).image).toBe("6.png");
+      expect(new Card({ rank: "8", figura: "Kier"}).image).toBe("27.png");
+      expect(new Card({ rank: "Ace", figura: "Karo"}).image).toBe("4.png");
+      expect(new Card({ rank: "2", figura: "Trefl"}).image).toBe("49.png");
+  });
+
+  it("should map its color to black or red based on suit", function () {
+      expect(new Card({ figura: "Pik"}).color).toBe("black");
+      expect(new Card({ figura: "Kier"}).color).toBe("red");
+      expect(new Card({ figura: "Karo"}).color).toBe("red");
+      expect(new Card({ figura: "Trefl"}).color).toBe("black");
+  });
+
+});
---
+++ tests/cards/deck_test.js
@@ -0,0 +1,48 @@
+describe("A deck of cards", function () {
+  "use strict";
+
+  it("should have 52 cards", inject(function () {
+    expect(new Deck().unShuffled().length).toBe(52);
+  }));
+
+  it("should start with aces", inject(function () {
+    var cards = new Deck().unShuffled();
+
+    expect(cards[0].rank).toEqual("Ace");
+    expect(cards[0].figura).toEqual("Trefl");
+
+    expect(cards[1].rank).toEqual("Ace");
+    expect(cards[1].figura).toEqual("Pik");
+
+    expect(cards[2].rank).toEqual("Ace");
+    expect(cards[2].figura).toEqual("Kier");
+
+    expect(cards[3].rank).toEqual("Ace");
+    expect(cards[3].figura).toEqual("Karo");
+  }));
+
+  it("should end with twos", inject(function () {
+    var cards = new Deck().unShuffled();
+
+    expect(cards[48].rank).toEqual("2");
+    expect(cards[48].figura).toEqual("Trefl");
+
+    expect(cards[49].rank).toEqual("2");
+    expect(cards[49].figura).toEqual("Pik");
+
+    expect(cards[50].rank).toEqual("2");
+    expect(cards[50].figura).toEqual("Kier");
+
+    expect(cards[51].rank).toEqual("2");
+    expect(cards[51].figura).toEqual("Karo");
+  }));
+
+  it("should shuffle", inject(function () {
+    var unShuffled = new Deck().unShuffled();
+
+    var shuffled = new Deck().shuffled();
+
+    expect(shuffled).not.toEqual(unShuffled);
+  }));
+
+});
