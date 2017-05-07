53b867830ad06b16289ca362bc61cb60f0732e96
marek kacprzak <marek.kacprzak@windowslive.com>
2017-05-07 13:01:09 +0100

    refactoring Card into GlobalModuleFormat (encapsulated into default Card) + Desck into ESMF becouse of import keyworld

---
+++ app/cards/card.js
@@ -0,0 +1,17 @@
+function Card(card) {
+    this.figura = card.figura;
+    this.rank = card.rank;
+    this.image = (Card.ranksInImagesOrder.indexOf(this.rank) * 4 + Card.suitsInImagesOrder.indexOf(this.figura) + 1) + ".png";
+    this.color = card.figura === "Pik" || card.figura === "Trefl" ? "black" : "red";
+    this.turnedUp = false;
+}
+
+Card.ranksInImagesOrder = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+Card.suitsInImagesOrder = ["Trefl", "Pik", "Kier", "Karo"];
+
+Card.prototype.turnUp = function() {
+    this.turnedUp = true;
+};
+Card.prototype.turnDown = function() {
+    this.turnedUp = false;
+};
