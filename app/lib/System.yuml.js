808c398d24b1dcc95126eb1755899973e72372a8
marek kacprzak <marek.kacprzak@windowslive.com>
2017-05-07 13:13:55 +0100

    module export UML - refaktoring

--- app/lib/System.yuml.js
+++ app/lib/System.yuml.js
@@ -26,6 +26,9 @@ window.showModuleRelationships = function() {
             var name = displayName(module.name);
 
             var dependencies = module.deps
+                .map(function(dependency) {
+                    return System.normalizeSync(dependency, module.name, module.address)
+                })
                 .map(displayName)
                 .map(function(dependencyName) {
                     return "[" + name + "]->[" + dependencyName + "]"
