05dd298c4bc99346d8632f4105c6e75a6999f5a2
marek kacprzak <marek.kacprzak@windowslive.com>
2017-05-07 10:02:08 +0100

    refaktoring module - move 3 import into klondike module add service declaration into that module

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
-import "klondike/game.js";
-import "klondike/board.js";
 import "klondike/klondike.js";
-import "klondike/scoring.js";
 
 angular.module("solitaire", ["klondike", "ngDraggable"]);
+
 angular.element(document).ready(() => {
     angular.bootstrap(document, ["solitaire"]);
 });
