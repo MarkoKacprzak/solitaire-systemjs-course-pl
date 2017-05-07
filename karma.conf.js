39ecb73fcc751cf4ed5a8bb720753e90309126a1
marek kacprzak <marek.kacprzak@windowslive.com>
2017-05-07 15:33:25 +0100

    migrate karma to systemjs migrate 2 tests to module tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,59 @@
 /* eslint-env node*/
 
-module.exports = function (config) {
-  "use strict";
+module.exports = function(config) {
+    "use strict";
 
-  config.set({
+    config.set({
 
-    basePath: "./",
+        basePath: "./",
 
-    files: [
-      "app/bower_components/angular/angular.js",
-      "app/bower_components/angular-route/angular-route.js",
-      "app/bower_components/angular-mocks/angular-mocks.js",
-      "app/bower_components/underscore/underscore.js",
-      "app/cards/**/*.js",
-      "app/klondike/piles/pile.js",
-      "app/klondike/piles/foundationPile.js",
-      "app/klondike/piles/remainderPile.js",
-      "app/klondike/piles/tableauPile.js",
-      "app/klondike/*.js",
-      "tests/**/*.js"
-    ],
+        files: [
+            "app/bower_components/angular/angular.js",
+            "app/bower_components/angular-route/angular-route.js",
+            "app/bower_components/angular-mocks/angular-mocks.js",
+            "app/bower_components/underscore/underscore.js",
+            "app/klondike/piles/pile.js",
+            "app/klondike/piles/foundationPile.js",
+            "app/klondike/piles/remainderPile.js",
+            "app/klondike/piles/tableauPile.js"
 
-    autoWatch: true,
+            //"tests/**/*.js"
+        ],
 
-    frameworks: ["jasmine"],
+        proxies: {
+            "/node_modules": "/base/node_modules"
+        },
 
-    browsers: ["Chrome", "Firefox"],
+        jspm: {
+            config: "app/config.js",
+            packages: "app/bower_components/system.js/dist",
+            serveFiles: [
+                "app/**/*.js",
+                "node_modules/babel-core/browser.js"
+            ],
+            loadFiles: [
+                "tests/cards/card_test.js",
+            ]
+        },
 
-    plugins: [
-      "karma-chrome-launcher",
-      "karma-firefox-launcher",
-      "karma-jasmine",
-      "karma-junit-reporter"
-    ],
+        autoWatch: true,
 
-    junitReporter: {
-      outputFile: "test_out/unit.xml",
-      suite: "unit"
-    }
+        frameworks: ["jspm", "jasmine"],
 
-  });
+        browsers: ["Chrome"],
+
+        plugins: [
+            "karma-jspm",
+            "karma-chrome-launcher",
+            "karma-firefox-launcher",
+            "karma-jasmine",
+            "karma-junit-reporter"
+        ],
+
+        junitReporter: {
+            outputFile: "test_out/unit.xml",
+            suite: "unit"
+        }
+
+    });
 };
--- tests/cards/card_test.js
+++ tests/cards/card_test.js
@@ -1,18 +1,20 @@
-describe("A card", function () {
-  "use strict";
+import Card from "../../app/cards/card.js";
 
-  it("should link to the image of the suit and rank", function () {
-      expect(new Card({ rank: "King", figura: "Pik"}).image).toBe("6.png");
-      expect(new Card({ rank: "8", figura: "Kier"}).image).toBe("27.png");
-      expect(new Card({ rank: "Ace", figura: "Karo"}).image).toBe("4.png");
-      expect(new Card({ rank: "2", figura: "Trefl"}).image).toBe("49.png");
-  });
+describe("A card", function() {
+    "use strict";
 
-  it("should map its color to black or red based on suit", function () {
-      expect(new Card({ figura: "Pik"}).color).toBe("black");
-      expect(new Card({ figura: "Kier"}).color).toBe("red");
-      expect(new Card({ figura: "Karo"}).color).toBe("red");
-      expect(new Card({ figura: "Trefl"}).color).toBe("black");
-  });
+    it("should link to the image of the suit and rank", function() {
+        expect(new Card({ rank: "King", figura: "Pik" }).image).toBe("6.png");
+        expect(new Card({ rank: "8", figura: "Kier" }).image).toBe("27.png");
+        expect(new Card({ rank: "Ace", figura: "Karo" }).image).toBe("4.png");
+        expect(new Card({ rank: "2", figura: "Trefl" }).image).toBe("49.png");
+    });
+
+    it("should map its color to black or red based on suit", function() {
+        expect(new Card({ figura: "Pik" }).color).toBe("black");
+        expect(new Card({ figura: "Kier" }).color).toBe("red");
+        expect(new Card({ figura: "Karo" }).color).toBe("red");
+        expect(new Card({ figura: "Trefl" }).color).toBe("black");
+    });
 
 });
